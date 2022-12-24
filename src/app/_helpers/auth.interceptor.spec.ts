6f940214151c111e811622d9564ccfddb3f7cd58
jdbench <justin.d.bench@gmail.com>
2022-12-24 14:31:25 -0700

    delete all test files

--- src/app/_helpers/auth.interceptor.spec.ts
+++
@@ -1,8 +0,0 @@
-import { AuthInterceptorPipe } from './auth.interceptor';
-
-describe('AuthInterceptorPipe', () => {
-  it('create an instance', () => {
-    const pipe = new AuthInterceptorPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
